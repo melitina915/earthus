bb9c3c87e2147b3cf07c9142aa1c2105f106106c
meli <melitina915@gmail.com>
2023-12-30 02:49:37 +0900

    Initial commit

---
+++ src/main/java/com/gdscsmwu/solutionchallenge/earthus/EarthusApplication.java
@@ -0,0 +1,13 @@
+package com.gdscsmwu.solutionchallenge.earthus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdscsmwu/solutionchallenge/earthus/EarthusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdscsmwu.solutionchallenge.earthus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
